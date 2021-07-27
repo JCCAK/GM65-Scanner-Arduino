2259f22696b26e0fa3bafc0d65178c912bc49c4f
JckRowling <85646883+JckRowling@users.noreply.github.com>
2021-07-27 13:48:35 +0200

    Update GM65_Scan.ino

--- GM65_Scan.ino
+++ GM65_Scan.ino
@@ -1,34 +1,26 @@
-#include <Servo.h>
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  27.07.21    by Juan C aka JckRowling ;)
+//  Free of use and for educational purpouses, or not 
+//  No libraries needed and the UART2 is a hardware UART not a SoftSerial UART
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define BUTTON 0
 #define SERVO 25
 
 #define BUTTON 0 
-#define START_SCAN 7E000801000201ABCD   
-
+   
 //set the RX and TX pins for UART2  
 #define Tx 38                           
 #define Rx 39     
 
-
-Servo TrapServo;
-
-const uint8_t OPEN = 0;
-const uint8_t CLOSE = 90;
-
 String Data;
-char trigger[]= "7E000801000201ABCD" ;
+char trigger[]= "7E000801000201ABCD" ;  // Not used in this sketch, can be used after module setup for tigger command --> see GM65 Datasheet.
 
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(115200);
- Serial2.begin(9600, SERIAL_8N1, Tx, Rx);
+ Serial2.begin(9600, SERIAL_8N1, Tx, Rx); //init UART2 for Scanner DATA input
 
  pinMode(BUTTON,INPUT);
- pinMode(SERVO,OUTPUT);
- 
- TrapServo.attach(SERVO);
-
 }
 
 void loop() {
@@ -37,15 +29,13 @@ void loop() {
   TrapServo.write(CLOSE);
 
   if(!digitalRead(BUTTON)){
-    //Serial2.write(trigger);
+    //Serial2.write(trigger);  // Not used, not working
     Serial.println(trigger);
     }
   
   while(Serial2.available()>0){
     Data = Serial2.readString();
-    Serial.println(Data);
-    Serial.println(OPEN);
-    TrapServo.write(OPEN);  
+    Serial.println(Data); 
   }
 
   delay(1000);
